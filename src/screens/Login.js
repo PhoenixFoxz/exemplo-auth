ac4c01abbd6e793f93b3405b5c60dce2d92d6b23
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-19 08:09:33 -0300

    Initial commit

---
+++ src/screens/Login.js
@@ -0,0 +1,76 @@
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  TextInput,
+  Vibration,
+  View,
+} from "react-native";
+
+// importando os recursos de dutenticação
+import { auth } from "../../firebase.config";
+
+// importando a função de login com e-mail e senha
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const login = async () => {
+    if (!email || !senha) {
+      Alert.alert("atenção", "Preencha o email e senha");
+      Vibration.vibrate(300);
+    }
+
+    console.log(email, senha);
+  };
+
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.formulario}>
+        <TextInput
+          onChangeText={(valor) => setEmail(valor)}
+          placeholder="E-mail"
+          style={estilos.input}
+        />
+        <TextInput
+          onChangeText={(valor) => setSenha(valor)}
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+        />
+        <View style={estilos.botoes}>
+          <Button onPress={login} title="Entre" color="green" />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formulario: {
+    marginBottom: 32,
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 4,
+  },
+  botoes: {
+    marginVertical: 8,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
