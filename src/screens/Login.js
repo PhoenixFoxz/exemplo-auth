a262ce9e23ba0813bedac3f2b4f6f40517891957
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-03-19 09:50:39 -0300

    Colocando o keyboardType="email-address" e a validação

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -66,6 +66,7 @@ export default function Login({ navigation }) {
         <TextInput
           onChangeText={(valor) => setEmail(valor)}
           placeholder="E-mail"
+          keyboardType="email-address"
           style={estilos.input}
         />
         <TextInput
